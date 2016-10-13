7ea74546b54bf6187c3663c22784dc9b2f98eb40
Steve <stevezhanggeek@Xiaoyis-MacBook-Pro.local>
2016-10-13 14:38:02 -0700

    Init

---
+++ assets/js/grayscale.js
@@ -0,0 +1,45 @@
+/*!
+ * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery to collapse the navbar on scroll
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+$(document).ready(function() {
+  var owl = $("#owl-food");
+  owl.owlCarousel({
+    items : 6
+  });
+
+  for (var i=0; i<10; i++) {
+    var content = "<div class=\"img-container\"> <img class=\"img-responsive\" src=\"assets/images/food/"+i+".jpg\"></div>";
+    owl.data('owlCarousel').addItem(content);
+  }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $(this).closest('.collapse').collapse('toggle');
+});
